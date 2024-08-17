ed58c5dff3ba7282b7650b7a701eee33a2e26af6
Rose Maria Roy <rosemariaroy2024@mca.ajce.in>
2024-08-17 20:35:07 +0530

    commit

--- script/script.js
+++ script/script.js
@@ -55,4 +55,18 @@ document.querySelector('.hamburger').addEventListener('click', function() {
 });
 
 
-// slider
+//accrdion
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
